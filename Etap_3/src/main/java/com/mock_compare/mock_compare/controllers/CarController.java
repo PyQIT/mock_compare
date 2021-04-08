0f0c446a51bf51e6ece95aa6a5c7d7f5f752f86d
PyQIT <przempy@gmail.com>
2021-04-09 00:57:24 +0200

    car controller changes

--- Etap_3/src/main/java/com/mock_compare/mock_compare/controllers/CarController.java
+++ Etap_3/src/main/java/com/mock_compare/mock_compare/controllers/CarController.java
@@ -1,10 +1,17 @@
 package com.mock_compare.mock_compare.controllers;
 
+import com.mock_compare.mock_compare.services.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+@RequiredArgsConstructor
+@CrossOrigin
+public class CarController {
+
+    private final CarService carService;
 
     @GetMapping("/hello")
     public String getData(){
