8992d2b2cad4ad18f2163f52ec3cb7db04839dd6
PyQIT <przempy@gmail.com>
2021-04-09 01:09:41 +0200

    car controller created

--- Etap_3/src/main/java/com/mock_compare/mock_compare/controllers/CarController.java
+++ Etap_3/src/main/java/com/mock_compare/mock_compare/controllers/CarController.java
@@ -1,10 +1,15 @@
 package com.mock_compare.mock_compare.controllers;
 
+import com.mock_compare.mock_compare.dto.CarDto;
+import com.mock_compare.mock_compare.models.Car;
 import com.mock_compare.mock_compare.services.CarService;
+import com.mock_compare.mock_compare.transformer.CarTransformer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +18,37 @@ public class CarController {
 
     private final CarService carService;
 
-    @GetMapping("/hello")
-    public String getData(){
-        return "Hello there";
+    @GetMapping("/cars")
+    @ResponseStatus(HttpStatus.OK)
+    public List<CarDto> getCars(){
+        List<Car> cars = carService.getCars();
+        return cars.stream().map(CarTransformer::convertToDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("/cars/{brand}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<CarDto> getCars(@PathVariable String brand){
+        List<Car> cars = carService.getCars(brand);
+        return cars.stream().map(CarTransformer::convertToDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("/cars/{brand}/{model}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<CarDto> getCars(@PathVariable String brand, String model){
+        List<Car> cars = carService.getCars(brand, model);
+        return cars.stream().map(CarTransformer::convertToDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("/car/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CarDto getCar(@PathVariable Long id){
+        return CarTransformer.convertToDto(carService.getCar(id));
+    }
+
+    @PostMapping("/car")
+    @ResponseStatus(HttpStatus.OK)
+    public void createCar(@RequestBody CarDto carDto){
+        Car tmpCar = CarTransformer.convertToEntity(carDto);
+        carService.createCar(tmpCar);
     }
 }
