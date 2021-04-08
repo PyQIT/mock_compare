115984310fd994d73ff069f7660bf4040596901c
PyQIT <przempy@gmail.com>
2021-04-09 00:22:15 +0200

    Added model

---
+++ Etap_3/src/main/java/com/mock_compare/mock_compare/models/Car.java
@@ -0,0 +1,25 @@
+package com.mock_compare.mock_compare.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, insertable = false)
+    private Long id;
+
+    @Column
+    private String brand;
+
+    @Column
+    private String model;
+
+    @Column
+    private Boolean isAvailable;
+}
