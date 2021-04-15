71c67a22052dd7ba09ae53c99bcedfa84bf7f4c1
PyQIT <przempy@gmail.com>
2021-04-15 23:39:05 +0200

    cleanup

--- Etap_3/src/main/java/com/mock_compare/mock_compare/files/CsvImport.java
+++ Etap_3/src/main/java/com/mock_compare/mock_compare/files/CsvImport.java
@@ -14,11 +14,11 @@ import java.util.List;
 public class CsvImport {
 
     public List<String[]> convertCsvToArray(String csvFile) throws IOException, CsvException {
-        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build(); // custom separator
+        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
         try (CSVReader reader = new CSVReaderBuilder(
                 new FileReader(csvFile))
-                .withCSVParser(csvParser)   // custom CSV parser
-                .withSkipLines(1)           // skip the first line, header info
+                .withCSVParser(csvParser)
+                .withSkipLines(1)
                 .build()) {
             List<String[]> r = reader.readAll();
             r.forEach(x -> System.out.println(Arrays.toString(x)));
