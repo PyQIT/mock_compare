5814ce3a8f13f89546c5a0bfd2075917b8cb527d
PyQIT <przempy@gmail.com>
2021-05-12 00:12:54 +0200

    added tests and json dependency

--- Etap_3/src/test/java/com/mock_compare/mock_compare/services/CarServiceMockImplTest.java
+++ Etap_3/src/test/java/com/mock_compare/mock_compare/services/CarServiceMockImplTest.java
@@ -1,28 +1,105 @@
 package com.mock_compare.mock_compare.services;
 
 import com.mock_compare.mock_compare.models.Car;
+import com.mock_compare.mock_compare.repositories.CarRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.ArrayList;
+import java.util.Optional;
+
+@SpringBootTest
 public class CarServiceMockImplTest {
 
-    @InjectMocks
-    private CarServiceImpl carService;
+    @Autowired
+    private CarRepository carRepository;
+    private Optional Optional;
+
+    @BeforeEach
+    void setUp(){
+        carRepository.deleteAll();
+    }
+
+    @AfterEach
+    void tearDown(){
+        carRepository.deleteAll();
+    }
 
     @Test
     void getCarsTest(){
         Car car = new Car();
         car.setModel("E46");
         car.setBrand("BMW");
+        car.setId(5l);
+        car.setSalesman("Pracownik");
+        car.setIsAvailable(true);
 
-        carService.createCar(car);
-        carService.getCars();
-        carService.getCars("BMW");
-        carService.getCars("BMW", "E46");
-        carService.getCar(1L);
+        ArrayList<Car> cars = new ArrayList<Car>();
+        cars.add(car);
+
+        carRepository.saveAndFlush(car);
+
+
+        Assertions.assertEquals(cars, carRepository.findAll());
+    }
+
+    @Test
+    void getCarsByBrandTest(){
+        Car car = new Car();
+        car.setModel("E46");
+        car.setBrand("BMW");
+        car.setId(3l);
+        car.setSalesman("Pracownik");
+        car.setIsAvailable(true);
+
+        ArrayList<Car> cars = new ArrayList<Car>();
+        cars.add(car);
+
+        carRepository.saveAndFlush(car);
+
+
+        Assertions.assertEquals(cars, carRepository.getCarByBrand("BMW"));
+    }
+
+    @Test
+    void getCarsByBrandAndModelTest(){
+        Car car = new Car();
+        car.setModel("E46");
+        car.setBrand("BMW");
+        car.setId(2l);
+        car.setSalesman("Pracownik");
+        car.setIsAvailable(true);
+
+        ArrayList<Car> cars = new ArrayList<Car>();
+        cars.add(car);
+
+        carRepository.saveAndFlush(car);
+
+
+        Assertions.assertEquals(cars, carRepository.getCarByBrandAndModel("BMW","E46"));
+    }
+
+    @Test
+    void getCarsByIdTest(){
+        Car car = new Car();
+        car.setModel("E46");
+        car.setBrand("BMW");
+        car.setId(4l);
+        car.setSalesman("Pracownik");
+        car.setIsAvailable(true);
+
+        ArrayList<Car> cars = new ArrayList<Car>();
+        cars.add(car);
+
+        carRepository.saveAndFlush(car);
+
+        Optional<Car> opt = Optional.of(car);
+
+        Assertions.assertEquals(opt, carRepository.findById(4l));
     }
 
 }
