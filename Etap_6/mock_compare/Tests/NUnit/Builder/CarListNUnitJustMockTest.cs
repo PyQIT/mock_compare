bc466aa9d88acf589543c3d46774b2ba04eec51b
Krzysztof <fanatic1009@gmail.com>
2021-05-19 23:49:55 +0200

    Mock NUnit car list test.

--- Etap_6/mock_compare/Tests/NUnit/Builder/CarListNUnitJustMockTest.cs
+++ Etap_6/mock_compare/Tests/NUnit/Builder/CarListNUnitJustMockTest.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using mock_compare.Tests.NUnit;
+using NUnit.Framework;
+using Moq;
+using System.Collections;
+
 
 namespace mock_compare.Tests.NUnit.Builder
 {
+    [TestFixture]
     public class CarListNUnitJustMockTest
     {
+
+        [Test]
+        public void getCarList()
+        {
+
+            mock_compare.Builder.CarList carList = new Mock<mock_compare.Builder.CarList>();
+            Assert.AreEqual(carList.getCarList().Capacity, 5);
+
+
+        }
+
+        private List<mock_compare.Builder.CarList> setCar()
+        {
+
+            mock_compare.Builder.CarList carList = new mock_compare.Builder.CarList();
+            carList.initList();
+
+            return carList.getCarList();
+
+        }
+
     }
 }
