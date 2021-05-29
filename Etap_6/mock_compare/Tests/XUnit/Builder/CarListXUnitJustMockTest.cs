4832cb92cf06bb615c7ae62a219af71d009b3777
Krzysztof <fanatic1009@gmail.com>
2021-05-29 13:37:03 +0200

    CarList: Moq + JustMock

--- Etap_6/mock_compare/Tests/XUnit/Builder/CarListXUnitJustMockTest.cs
+++ Etap_6/mock_compare/Tests/XUnit/Builder/CarListXUnitJustMockTest.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections;
+using Telerik.JustMock;
+using Assert = Xunit.Assert;
+using Xunit;
 
 namespace mock_compare.Tests.XUnit.Builder
 {
     public class CarListXUnitJustMockTest
     {
+
+        [Fact]
+        public void getCarList()
+        {
+
+            mock_compare.Builder.CarList carList = Mock.Create(() => new mock_compare.Builder.CarList());
+            Assert.Equal(0, carList.getCarList().Capacity);
+
+
+        }
+
+        private List<mock_compare.Builder.CarList> setCar()
+        {
+
+            mock_compare.Builder.CarList carList = new mock_compare.Builder.CarList();
+            carList.initList();
+
+            return carList.getCarList();
+
+        }
+
     }
 }
--- Etap_6/mock_compare/Tests/XUnit/Builder/CarListXUnitMoqTest.cs
+++ Etap_6/mock_compare/Tests/XUnit/Builder/CarListXUnitMoqTest.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using mock_compare.Builder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Assert = Xunit.Assert;
+using Xunit;
 
 namespace mock_compare.Tests.XUnit.Builder
 {
     public class CarListXUnitMoqTest
     {
+
+        [Fact]
+        public void getCarList()
+        {
+            var carListMock = new Mock<CarList>();
+            CarList carList = new CarList(carListMock.Object);
+            Assert.Equal(0, carList.getCarList().Capacity);
+
+
+        }
+
+        private List<mock_compare.Builder.CarList> setCar()
+        {
+
+            mock_compare.Builder.CarList carList = new mock_compare.Builder.CarList();
+            carList.initList();
+
+            return carList.getCarList();
+
+        }
+
     }
 }
